965471a4e0cdde8dc509a471179ba5dc33803bce
Andrew_Herbedz <andrei.herbedz@gmail.com>
2022-09-29 20:34:20 +0300

    fix for Task_5

--- Home_work_5/Task_5.ts
+++ Home_work_5/Task_5.ts
@@ -1,6 +1,5 @@
-let result = 0;
-
 function getSum(a: number, b: number) {
+  let result = 0;
   if (a < b) {
     for (a; a < b; a++) {
       result += a;
@@ -13,4 +12,6 @@ function getSum(a: number, b: number) {
   }
 }
 
-console.log(getSum(-5, 3));
+console.log(getSum(-1, 2));
+console.log(getSum(-2, 4));
+console.log(getSum(-2, 2));
